475188c05d003825d1c18d0448840089dceeac4d
assispaola <paolasantos.assis@gmail.com>
2026-01-08 03:19:05 -0300

    fix: hash routing for github pages

--- evo-app/dist/evo-app/server/angular-app-manifest.mjs
+++ evo-app/dist/evo-app/server/angular-app-manifest.mjs
@@ -16,8 +16,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 14948, hash: '779fe8c9c78c949a8c9dc1dd146ec5ae532c6b68b1573649b5ae61b036635250', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 976, hash: '5cc6b5694ebee7e04bed9b16b3decd30f730b18c5c48a2f5dfe17eeb39aae4b0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 14948, hash: 'd2e8b50279f21bb6b55e9e1382c18eee09747239444dbc4a25eda457b4fe01c7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 976, hash: '07921badc5689fca4d08f76a9a29a5427d0e5c23d95687e5a74d7e1f095e2880', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-QMKLIQG5.css': {size: 240976, hash: 'rNKyye+NlAU', text: () => import('./assets-chunks/styles-QMKLIQG5_css.mjs').then(m => m.default)}
   },
 };
